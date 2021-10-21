241aa1c931bee7f4753ed671f5d7a1f57e65cfde
hanifrfd <hanif.wrong@gmail.com>
2021-10-21 16:04:36 +0700

    change news size

--- src/home/News/index.js
+++ src/home/News/index.js
@@ -11,21 +11,21 @@ export default function News() {
                 <Headline headline="News"></Headline>
                 <div className="news-list">
                     <Card type="news-card">
-                        <img src="news/1.jpg" width="100%" height="120" alt="news" />
+                        <img src="news/1.jpg" width="240" height="120" alt="news" />
                         <div className="news-content">
                             <h4 className="text-1 txt-black">Daftar Melalui aplikasi online</h4>
                             <p className="text-3 txt-primary">Jun, 2021</p>
                         </div>
                     </Card>
                     <Card type="news-card">
-                        <img src="news/2.jpg" width="100%" height="100" alt="news" />
+                        <img src="news/2.jpg" width="240" height="120" alt="news" />
                         <div className="news-content">
                             <h4 className="text-1 txt-black">Daftar Melalui aplikasi online</h4>
                             <p className="text-3 txt-primary">Jun, 2021</p>
                         </div>
                     </Card>
                     <Card type="news-card">
-                        <img src="news/3.jpg" width="100%" height="100" alt="news" />
+                        <img src="news/3.jpg" width="240" height="120" alt="news" />
                         <div className="news-content">
                             <h4 className="text-1 txt-black">Daftar Melalui aplikasi online</h4>
                             <p className="text-3 txt-primary">Jun, 2021</p>
